99ad76b949904c261927db77160b27c2d6bd89a2
TakeshiTseng <a86487817@gmail.com>
2014-02-04 16:38:21 +0800

    add 404 not found

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,17 @@ var client  = redis.createClient('6379', '127.0.0.1');
 
 exports.index = function(req, res){
 	var room = req.params.room;
+	if(room !== 'r0' && room !== 'r1' && room !== 'r2') {
+		res.status(404);
+
+		if (req.accepts('html')) {
+			res.render('404', { url: req.url });
+			res.type('txt').send('Not found');
+			res.end();
+			return;
+		}
+	}
+	
 	console.log("get : " + room);
 	var sitList;
 	client.get(room, function(err, reply) {
