73c2f4e4dcea41ccc3695f02298a6bc2de1bdb1e
Takeshi <a86487817@gmail.com>
2015-03-03 10:48:58 +0000

    ignore sitcon bot message

--- app.js
+++ app.js
@@ -63,7 +63,8 @@ client.join(config.irc.channel + ' ' + config.irc.bot_pwd)
 
 client.addListener('message', function (from, to, message) {
 	winston.info("[IRC] channel : " + config.irc.channel + " from : " + from + ', to : ' + to + ', message : ' + message);
-	if(!to.match(/HITCON_BOT[0-9]?/)) {
+	
+	if(!to.match(/SITCON_BOT[0-9]?/)) {
 		message = escape(message);
 		io.sockets.emit('irc_msg', {'from':from, 'to': to, 'msg':message});
 	}
