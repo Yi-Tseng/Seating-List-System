5a954bfdd61805869fe1ddef665c9251cb9e6e6b
Takeshi <a86487817@gmail.com>
2016-02-10 17:38:23 +0800

    Move admin post function
    from app.js to route/admin.js

--- app.js
+++ app.js
@@ -59,33 +59,7 @@ io.on('connection', function (socket) {
 
 // path
 app.get('/admin', admin.index);
-app.post('/admin', function(req, res) {
-
-	var tmp = req.body.pwd;
-	if(typeof tmp === 'undefined'){
-		return;
-	}
-
-	for(var c=0; c<128; c++) {
-		tmp = md5(tmp);
-	}
-
-	if(tmp !== config.admin_pwd) {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    winston.info('[/admin] Password error from ip ' + ip);
-		res.send({res:'err'});
-		res.end();
-	} else {
-		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-		winston.info('[/admin] Password pass from ip ' + ip);
-		winston.info('[/admin] message : ' + req.body.conf_msg);
-
-		io.sockets.emit('conf_msg', {'msg':req.body.conf_msg});
-		res.send({res:'ok'})
-		res.end()
-	}
-});
-
+app.post('/admin', admin.postMsg);
 user.setSockets(io.sockets);
 app.get('/list', user.list);
 app.post('/modify', user.modify);
